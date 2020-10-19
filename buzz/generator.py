c9d791b36d3e184a45b567e85713e09e6ae987bb
farnazdargahi <fdargahi@bcit.ca>
2020-10-19 22:41:27 +0000

    Step 9

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
+buzz = ('Farnaz Dargahi''A00123456''continuous testing', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',

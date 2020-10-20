2bbd261fb73e9a48ff3b32c8d7a8aff7040a6222
farnazdargahi <fdargahi@bcit.ca>
2020-10-20 00:00:22 +0000

    step 14

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import random
 
-buzz = ('Farnaz','khar','continuous deployment', 'feri' )
+buzz = ('Farnaz','continuous deployment', 'feri' )
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
